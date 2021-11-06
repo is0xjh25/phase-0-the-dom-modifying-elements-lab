571376af7ed91d6f4f2379f03ae2318ae08840f8
Yun-Chi Hsiao <is0.jimhsiao@gmail.com>
2021-11-06 10:35:43 +0800

    Update: Test passed

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const ele = document.querySelector("main#main");
+ele.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "YOUR-NAME is the champion";
+document.body.appendChild(newHeader);
